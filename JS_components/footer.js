d55bce63561a79d669e05c125b4edd64079f36db
Stephane Simcic <simcic.stephane@gmail.com>
2022-04-20 19:40:06 +0800

    Resets the navbar elements with proper positions. Adds scroll-down

---
+++ JS_components/footer.js
@@ -0,0 +1,19 @@
+console.log("hello from footer")
+
+// FOOTER DISAPPEAR ON SCROLL DOWN
+const footerDisappear = () => {
+  const footer = document.querySelector(".footer");
+
+  if (footer) {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 350) {
+        footer.classList.add("footer");
+      }
+      if (window.scrollY <= 350) {
+        footer.classList.remove("footer");
+      }
+    })
+  } 
+};
+
+footerDisappear();
